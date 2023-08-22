9e39804d33b684e775f8d942aa76c68973e3f475
jackira01 <s4bu3z0@gmail.com>
2023-08-22 12:58:14 -0500

    added readme and update code

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -7,32 +7,37 @@ notesCtrl.renderNoteForm = (req, res) => {
 
 notesCtrl.createNewNote = async (req, res) => {
   const { title, description } = req.body;
-  const newNote = new Note({ title, description });
+  const newNote = new Note({ title, description, user: req.user._id });
+  console.log(newNote);
   await newNote.save();
-  req.flash('success_msg', 'Note added successfully')
+  req.flash('success_msg', 'Note added successfully');
   res.redirect('/notes');
 };
 
 notesCtrl.renderNotes = async (req, res) => {
-  const notes = await Note.find().lean();
+  const notes = await Note.find({ user: req.user._id }).lean();
   res.render('notes/all-notes', { notes });
 };
 
 notesCtrl.renderEditForm = async (req, res) => {
   const note = await Note.findById(req.params.id).lean();
+  if (note.user != req.user._id) {
+    req.flash('error_msg', 'Access Denied');
+    res.redirect('/notes');
+  }
   res.render('notes/edit-note', { note });
 };
 
 notesCtrl.updateNotes = async (req, res) => {
   const { title, description } = req.body;
   await Note.findByIdAndUpdate(req.params.id, { title, description });
-  req.flash('success_msg', 'Note updated successfully')
+  req.flash('success_msg', 'Note updated successfully');
   res.redirect('/notes');
 };
 
 notesCtrl.deleteNotes = async (req, res) => {
   await Note.findByIdAndDelete(req.params.id);
-  req.flash('success_msg', 'Note deleted successfully')
+  req.flash('success_msg', 'Note deleted successfully');
   res.redirect('/notes');
 };
 
